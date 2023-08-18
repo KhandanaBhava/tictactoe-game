655c71715497158a7b3b94f231f01a57cf139286
Aratrik <aratrikmukherjee2000@gmail.com>
2023-08-19 00:26:11 +0530

    Added new game button

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,10 @@ function App() {
   const moveTo = move => {
     setCurrentMove(move);
   };
+  const onNewGameStart = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
   return (
     <div className="app">
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
@@ -51,6 +55,13 @@ function App() {
         squares={gamingBoard.squares}
         handleSquareClick={handleSquareClick}
       />
+      <button
+        type="button"
+        onClick={onNewGameStart}
+        className={`btn-reset ${winner ? 'active' : ''}`}
+      >
+        Start new game
+      </button>
       <h2>Current game history</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
