113cd3bf47c0e44379b71b552cf1aa1118678008
Aratrik <aratrikmukherjee2000@gmail.com>
2023-08-18 18:00:40 +0530

    introduced a player

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,17 +2,21 @@ import { useState } from 'react';
 import Square from './Square';
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  console.log(squares);
+  const [isXNext, setIsXNext] = useState(false);
 
   const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition]) {
+      return;
+    }
     setSquares(currentSquares => {
       return currentSquares.map((squareValue, position) => {
         if (clickedPosition === position) {
-          return 'X';
+          return isXNext ? 'X' : '0';
         }
         return squareValue;
       });
     });
+    setIsXNext(currentIsXNext => !currentIsXNext);
   };
   const renderSquare = position => {
     return (
